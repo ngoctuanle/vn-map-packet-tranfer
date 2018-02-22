f6ca7400193b2f48a62d0f337eb37d57ab44eb05
Le Ngoc Tuan <ngoctuanle020395@gmail.com>
2018-02-22 19:03:00 +0700

    add bootstrap offline and d3 js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
+	render() {
+		return (
+			<div className="App">
 
-      </div>
-    );
-  }
+			</div>
+		);
+	}
 }
 
 export default App;
